f787778918b148bc20bfda27d7d837260a3cbe1b
leducanh2410 <57927000+leducanh2410@users.noreply.github.com>
2024-06-08 21:29:39 +0700

    initial commit

---
+++ src/main/java/org/example/datnbackend/DatnBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.datnbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatnBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/datnbackend/DatnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.datnbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
